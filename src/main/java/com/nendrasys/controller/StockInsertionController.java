01935285a84d7340db6199564d05845cfbe35f72
DESKTOP-H0PC59H\MAHINDRA <mahimahendra603@gmail.com>
2021-05-05 18:25:29 +0530

    updated Stock Insertion Controller

--- src/main/java/com/nendrasys/controller/StockInsertionController.java
+++ src/main/java/com/nendrasys/controller/StockInsertionController.java
@@ -6,6 +6,7 @@ import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -30,7 +31,7 @@ public class StockInsertionController {
     }
 
     @RequestMapping(value = "/saveCompany", method = RequestMethod.POST)
-    public String saveCompany(StockDetailsModel stockDetailsModel) {
+    public String saveCompany(@ModelAttribute("stockDetailsModel") StockDetailsModel stockDetailsModel) {
         logger.info("Inside saveCompany method at StockInsertionController class");
         stockDetailsService.addStockToMarketList(stockDetailsModel);
         return "saveCompanySuccessPage";
