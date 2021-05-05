3baba6b0e194753043b75a811a258a76ed6d8eec
balajisaravanannc <balaji@nendrasys.com>
2021-05-05 11:57:05 +0530

    add

--- src/main/java/com/nendrasys/repository/StockDetailsRepository.java
+++ src/main/java/com/nendrasys/repository/StockDetailsRepository.java
@@ -2,9 +2,8 @@ package com.nendrasys.repository;
 
 import com.nendrasys.model.StockDetailsModel;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
+import org.springframework.data.jpa.repository.Query;
 import java.io.Serializable;
 import java.util.List;
 
